66ade611be65680b5c42509b2128daa4d9cf1cda
Josué Ayala <ayalavalencia11@gmail.com>
2022-09-14 08:45:50 -0600

    docs: ✏️ Remove unused comments

--- src/all/pc/shared/EquipmentLaptop.java
+++ src/all/pc/shared/EquipmentLaptop.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package all.pc.shared;
 
-/**
- *
- * @author Cartagena
- */
 public class EquipmentLaptop extends Equipment {
 
     private String memory;
