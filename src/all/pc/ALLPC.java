4bf170039657a3bf3d8d68e97aa92aafd893d73a
Cartagena2001 <salvadorcartajena@hotmail.com>
2022-09-14 00:02:02 -0600

    show JOptionPane

--- src/all/pc/ALLPC.java
+++ src/all/pc/ALLPC.java
@@ -9,12 +9,23 @@ public class ALLPC {
     public static void main(String[] args) {
         
         ArrayList<EquipmentDesktop> equipments = new ArrayList<EquipmentDesktop>();
-
         EquipmentDesktop eq = new EquipmentDesktop();
-        eq.setDisk_capacity("hola");
         
-        eq.setMaker("Dell");
         System.out.println(eq.getDisk_capacity_size());
+        eq.setMaker(JOptionPane.showInputDialog(null, "Enter Manufacturer"));
+        eq.setModel(JOptionPane.showInputDialog(null, "Enter Model"));
+        eq.setProcessor(JOptionPane.showInputDialog(null, "Enter MicroProcessor"));
+        eq.setMemory(JOptionPane.showInputDialog(null, "Enter Memory"));
+        eq.setGraphic_card(JOptionPane.showInputDialog(null, "Enter Graphic card"));
+        eq.setTower_size(JOptionPane.showInputDialog(null, "Enter Tower Size"));
+        eq.setDisk_capacity(JOptionPane.showInputDialog(null, "Enter Disk Capacity"));
+        
+        int n =1;
+        String msg ="";
+        for(int i=0 ; i < n ; i++){
+            msg += "Desktops(" + (i+1) + ")" +"\nMaker  = " +eq.getMaker() +"\nModel  = " +eq.getModel() +"\nProcessor  = " +eq.getProcessor() +"\nMemory  = " +eq.getMemory() +"\nGraphic Card  = " +eq.getGraphic_card() +"\nTower size  = " +eq.getTower_size() +"\nDisk capacity  = " +eq.getDisk_capacity_size() + "\n--------------------------------\n";
+        }
+        JOptionPane.showMessageDialog(null, msg);
     }
     
 }
